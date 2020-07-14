af7c991fe137aaaccb7c09cabe8cdc559cec8bb9
Ronny Jaramillo <ronny_jaramillo123@hotmail.es>
2020-07-14 08:59:10 -0500

    Introduccion Spyder

---
+++ 03 - Pandas/a_introduccion.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 14 07:41:14 2020
+
+@author: nebelschwaden
+"""
+
+print('Hola Spyder')
+nombre = "Ronny"
+numero = 3
+lista = [1,2,3]
+tupla = (4,5,6)
+diccionario = {
+    "nombre":"Ronny",
+    "apellido":"Jaramillo"
+    }
